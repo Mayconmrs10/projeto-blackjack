2343c5859703a6fd59414984110fc3bc7bf4fa23
unknown <m.resendemrs10@gmail.com>
2023-04-07 21:39:49 -0300

    adiciona parte 1 com projeto basico

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,30 @@
  * 
  * 
  */
+//INICIO
+alert ("Boas Vindas ao jogo de Blck Jack!")
+var nome = prompt('Qual o seu nome ?')
+var inicio = confirm('Ola, ' + nome + '! Vamos jogar uma partida?')
+
+if(inicio){
+   var cartaJogador1 = comprarCarta()
+   var cartaJogador2 = comprarCarta()
+   var cartaBanca1 = comprarCarta()
+   var cartaBanca2 = comprarCarta()
+   var pontosJogador = cartaJogador1.valor + cartaJogador2.valor 
+   var pontosBanca = cartaBanca1.valor + cartaBanca2.valor 
+
+   console.log(`${nome}- Cartas:${cartaJogador1.texto} ${cartaJogador2.texto} - ${pontosJogador}`)
+   console.log(`Banca - Cartas: ${cartaBanca1.texto} ${cartaBanca2.texto} - ${pontosBanca}` )
+
+   if(pontosJogador > pontosBanca){
+      console.log(`PARABENS!!! ${nome},  Você Ganhou!!!` )
+   }else if (pontosJogador < pontosBanca){
+      console.log("A BANCA VENCEU.")
+   }else{
+      console.log("EMPATE!!!")
+   }
+}else {
+   console.log ("FIM DE JOGO")
+}
+//FIM
